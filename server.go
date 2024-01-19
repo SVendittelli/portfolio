b3411cd50edd0733af19b4d754d6d2ae7ae4c3f7
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-01-19 16:40:45 +0000

    Add htmx for simple command interactivity

--- server.go
+++ server.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/template/handlebars/v2"
 )
+
+type Body struct {
+	Text string `form:"text"`
+}
 
 func main() {
 	engine := handlebars.New("./templates", ".hbs")
@@ -23,7 +28,27 @@ func main() {
 		return c.Render("index", fiber.Map{
 			"Page": "~",
 		}, "layout")
-	})
+	}).Name("index")
+
+	app.Post("/command", func(c *fiber.Ctx) error {
+		body := new(Body)
+
+		if err := c.BodyParser(body); err != nil {
+			return err
+		}
+
+		cmd := strings.Trim(body.Text, " ");
+
+		if cmd == "" {
+			return c.SendString("")
+		}
+
+		if strings.HasPrefix(cmd, "ls") {
+			return c.Render("commands/ls", fiber.Map{})
+		}
+
+		return c.SendString(strings.Split(cmd, " ")[0] + ": command not found")
+	}).Name("command")
 
 	log.Fatal(app.Listen(":3000"))
 }
